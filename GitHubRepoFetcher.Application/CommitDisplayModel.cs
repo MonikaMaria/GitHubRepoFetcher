86b80363e3e5380f7e990e659a1f4a5fc84fe68c
Monika Torkowska <monika.m.torkowska@gmail.com>
2024-12-05 20:44:43 +0100

    change display model

--- GitHubRepoFetcher.Application/CommitDisplayModel.cs
+++ GitHubRepoFetcher.Application/CommitDisplayModel.cs
@@ -1,10 +1,9 @@
-﻿namespace GitHubRepoFetcher.Application
+﻿namespace GitHubRepoFetcher.Application;
+
+public record CommitDisplayModel(string RepositoryName, string Sha, string Message, string CommitterName, DateTimeOffset CommittedAt)
 {
-    public record CommitDisplayModel(string RepositoryName, string Sha, string Message, string Commiter)
+    public override string ToString()
     {
-        public override string ToString()
-        {
-            return $"[{RepositoryName}]/[{Sha}]: {Message} [{Commiter}]";
-        }
+        return $"[{RepositoryName}]/[{Sha}]: {Message} [{CommitterName}]";
     }
 }
