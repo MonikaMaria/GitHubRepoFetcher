cdba99f712475a8767f609059d512039dafbe18f
Monika Torkowska <monika.m.torkowska@gmail.com>
2024-12-05 20:40:56 +0100

    change github client

---
+++ GitHubRepoFetcher.Application/GitHubApi.cs
@@ -0,0 +1,21 @@
+﻿using Refit;
+
+namespace GitHubRepoFetcher.Application;
+
+[Headers(
+    "User-Agent: GitHubRepoFetcher",
+    //"Authorization: Bearer",
+    "Accept: application/vnd.github+json", 
+    "X-GitHub-Api-Version: 2022-11-28"
+)]
+public interface IGitHubApi
+{
+    [Get("/users/{userName}")]
+    Task<ApiResponse<string>> GetUserAsync(string userName, CancellationToken cancellationToken);
+
+    [Get("/repos/{userName}/{repositoryName}")]
+    Task<ApiResponse<string>> GetRepositoryAsync(string userName, string repositoryName, CancellationToken cancellationToken);
+
+    [Get("/repos/{userName}/{repositoryName}/commits?per_page=10")] // Parameterize page size if needed
+    Task<ApiResponse<IEnumerable<GitHubCommitItem>>> GetCommitsAsync(string userName, string repositoryName, int page, CancellationToken cancellationToken);
+}
